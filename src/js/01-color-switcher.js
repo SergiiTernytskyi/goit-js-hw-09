d517a9f58b7d15e3e91ac65ee07e46fa6804143f
Sergii Ternytskyi <serhij86@gmail.com>
2022-09-30 22:19:39 +0300

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,33 @@
+refs = {
+  body: document.querySelector("body"),
+  startBtn: document.querySelector("[data-start]"),
+  stopBtn: document.querySelector("[data-stop]"),
+};
 
+console.log(refs.startBtn);
+console.log(refs.stopBtn);
+
+let intervalId = null;
+refs.stopBtn.disabled = true;
+
+refs.startBtn.addEventListener("click", () => {
+  refs.body.style.backgroundColor = getRandomHexColor();
+
+  refs.startBtn.disabled = true;
+  refs.stopBtn.disabled = false;
+
+  intervalId = setInterval(() => {
+    refs.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+});
+
+refs.stopBtn.addEventListener("click", () => {
+  refs.startBtn.disabled = false;
+  refs.stopBtn.disabled = true;
+
+  clearInterval(intervalId);
+});
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
